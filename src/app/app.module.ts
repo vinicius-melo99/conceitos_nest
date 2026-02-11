b9f763b597ddc8cae4cb980ed7533391851510bd
Vinicius Melo <viniciusmelo317@gmail.com>
2026-02-11 18:19:45 -0300

    aprendendo sobre modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConceptsManualModule } from 'src/concepts-manual/concepts-manual.module';
+import { ConceptsAutoModule } from 'src/concepts-auto/concepts-auto.module';
 
 @Module({
-  imports: [],
+  imports: [ConceptsManualModule, ConceptsAutoModule],
   controllers: [AppController],
   providers: [AppService],
 })
