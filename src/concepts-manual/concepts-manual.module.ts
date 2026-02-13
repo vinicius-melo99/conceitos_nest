040dc6653ee8b70ee054cc4e7f461fd9fc8bbfbb
Vinicius Melo <viniciusmelo317@gmail.com>
2026-02-13 07:51:53 -0300

    aula

--- src/concepts-manual/concepts-manual.module.ts
+++ src/concepts-manual/concepts-manual.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConceptsManualController } from './concepts-manual.controller';
+import { ConceptsManualService } from './concepts-manual.service';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
-  exports: [],
+  controllers: [ConceptsManualController],
+  providers: [ConceptsManualService],
 })
 export class ConceptsManualModule {}
