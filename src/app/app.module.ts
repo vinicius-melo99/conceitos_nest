748fa588b8c62e5d21fbff044d65df4db44df49e
Vinicius Melo <viniciusmelo317@gmail.com>
2026-02-13 09:38:03 -0300

    limpa projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConceptsManualModule } from 'src/concepts-manual/concepts-manual.module';
-import { ConceptsAutoModule } from 'src/concepts-auto/concepts-auto.module';
 
 @Module({
-  imports: [ConceptsManualModule, ConceptsAutoModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/concepts-manual/concepts-manual.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConceptsManualController } from './concepts-manual.controller';
-import { ConceptsManualService } from './concepts-manual.service';
-
-@Module({
-  controllers: [ConceptsManualController],
-  providers: [ConceptsManualService],
-})
-export class ConceptsManualModule {}
